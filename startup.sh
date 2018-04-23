4b3e5adfea4b63ddff534c0455a71e1d569a854a
hunterlong <info@socialeck.com>
2018-04-23 15:29:34 -0700

    new inputs

--- startup.sh
+++ startup.sh
@@ -22,21 +22,25 @@ bitcoin-cli -regtest importprivkey "cQrY4VypAuemJtHmNNJLyx1SNjY7mpfkdQEJpccpLSva
 
 bitcoin-cli -regtest $AUTH generate 1
 
-bitcoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 100
-bitcoin-cli -regtest sendfrom "" "mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW" 12
-bitcoin-cli -regtest sendfrom "" "mtdVMhiWWmegkkBhzYDrz84yfgofPNLNmb" 14
-bitcoin-cli -regtest sendfrom "" "mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5" 17
-bitcoin-cli -regtest sendfrom "" "mnk2URqujBqMEfhALMby1WZHoBRauW37Kg" 20
+bitcoin-cli -regtest setaccount "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" "Account1"
+bitcoin-cli -regtest setaccount "mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW" "Account2"
+bitcoin-cli -regtest setaccount "mtdVMhiWWmegkkBhzYDrz84yfgofPNLNmb" "Account3"
+bitcoin-cli -regtest setaccount "mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5" "Account4"
+bitcoin-cli -regtest setaccount "mnk2URqujBqMEfhALMby1WZHoBRauW37Kg" "Account5"
+
+bitcoin-cli -regtest sendmany "" '{"mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo":123.12345,"mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW":25.4112}'
+bitcoin-cli -regtest sendmany "" '{"mtdVMhiWWmegkkBhzYDrz84yfgofPNLNmb":62.4322,"mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5":31.2345}'
+bitcoin-cli -regtest sendmany "" '{"mnk2URqujBqMEfhALMby1WZHoBRauW37Kg":14.1337}'
+
+bitcoin-cli -regtest $AUTH generate 3
+
+bitcoin-cli -regtest sendmany "Account2" '{"mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo":1.1337,"mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5":4.123, "mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW":6.324}'
+bitcoin-cli -regtest sendmany "Account3" '{"mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5":2.1337,"mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW":6.123, "mtdVMhiWWmegkkBhzYDrz84yfgofPNLNmb": 7.34}'
+bitcoin-cli -regtest sendmany "Account4" '{"mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo":7.1337,"mnk2URqujBqMEfhALMby1WZHoBRauW37Kg":1.123}'
 
 bitcoin-cli -regtest $AUTH generate 1
 
-bitcoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 1
-bitcoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 2
-bitcoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 3
-
-bitcoin-cli -regtest $AUTH generate 1
-
-bitcoin-cli -regtest $AUTH getbalance
+bitcoin-cli -regtest $AUTH listaccounts
 
 bitcoin-cli -regtest $AUTH stop
 
